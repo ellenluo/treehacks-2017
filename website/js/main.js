81de261cc7b054a129002353bdb530f4c5c4b95d
ellenluo <ellenyilan.luo@gmail.com>
2017-02-18 17:40:07 -0800

    Added changes.

--- website/js/main.js
+++ website/js/main.js
@@ -1,36 +1,39 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     //location.href = 'index.html#start';
-    var pages = new Array();
+    var pages = [];
     var current = 0;
-    var loaded = new Array();
+    var loaded = [];
+    var pets = [];
 
     //get all the pages link inside the #pages div and fill an array
-    $('#pages a').each(function(index) {
+    $('#pages a').each(function (index) {
         pages[index] = $(this).attr('href');
         loaded[$(this).attr('href')] = 0;
     });
 
+    console.log(pages);
+
     //on scroll gets when bottom of the page is reached and calls the function do load more content
-    $(window).scroll(function(e){
-        if($(window).scrollTop() + $(window).height() >= $(document).height() - 300) {
+    $(window).scroll(function (e) {
+        if ($(window).scrollTop() + $(window).height() >= $(document).height() - 300) {
             console.log("bottom of the page reached!");
+            loaded[pages[current + 1]] = loaded[pages[current + 1]] + 1;
 
-            loaded[pages[current+1]] = loaded[pages[current+1]] + 1;
-
-            if(loaded[pages[current+1]] <= 1)
-                loadMoreContent(current+1);
+            if (loaded[pages[current + 1]] <= 1)
+                loadMoreContent(current + 1);
         }
     });
 
     //loads the next page and append it to the content with a fadeIn effect.
     function loadMoreContent(position) {
         //try to load more content only if the counter is minor than the number of total pages
-        if(position < pages.length) {
-            $('#loader').fadeIn('slow', function() {
-                $.get(pages[position], function(data) {
-                    $('#loader').fadeOut('slow', function() {
+        if (position < pages.length) {
+            $('#loader').fadeIn('slow', function () {
+                $.get(pages[position], function (data) {
+                    $('#loader').fadeOut('slow', function () {
                         $('#scroll-container').append(data).fadeIn(999);
-                        current=position;
+                        updateItems();
+                        current = position;
                     });
                 });
             });
@@ -42,31 +45,26 @@ jQuery(document).ready(function() {
         dataType: 'jsonp',
         url: "http://api.petfinder.com/pet.find?key=150d5bff08a3c97e69c2417d726c084d&location=94720&format=json&count=100",
         data: "",
-        success: function(data) {
-            var pets = [];
-            $.each(data.petfinder.pets.pet, function(i, obj) {
+        success: function (data) {
+            pets = [];
+            $.each(data.petfinder.pets.pet, function (i, obj) {
                 pets.push(obj);
             });
-            updateItems(pets);
+            updateItems();
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             console.log(errorThrown);
         }
     });
 
-    // generate content pages
-    function generatePages(numEntries) {
-
-    }
-
     // update pet items
-    function updateItems(pets) {
+    function updateItems() {
         $('.item').each(function (i, obj) {
             var pet = pets[i];
             console.log(pet);
 
             var name = pet.name.$t;
-            $(obj).find("h4").text(name);
+            $(obj).find("a").text(name);
 
             // set photo
             var photo = '';
@@ -87,7 +85,7 @@ jQuery(document).ready(function() {
             var breeds = pet.breeds.breed;
             if (isArray(breeds)) {
                 $.each(breeds, function (i, obj) {
-                    details = details + obj.$t + " ";
+                    details = details + obj.$t + " | ";
                 });
             } else {
                 details = breeds.$t;
@@ -95,7 +93,6 @@ jQuery(document).ready(function() {
             $(obj).find("p").text(details);
         });
     }
-    console.log(pages);
 
     // check if JSON is object or array
     function isArray(what) {
