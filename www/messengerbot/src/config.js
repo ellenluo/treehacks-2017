29e3cd276cd43a50c3dda7edd62c0b8ead9e43dd
ellenluo <ellenyilan.luo@gmail.com>
2017-02-19 01:54:59 -0800

    Server

--- www/messengerbot/src/config.js
+++
@@ -1,6 +0,0 @@
-var messengerPageToken = process.env.MESSENGER_TOKEN || 'EAAF7SFVHWyABAJs1tMKb9WBopzrbLHFmTZBSeFinE0jEYDjm5AAnMdo2FrZBxBupaUZAxRdMZCzJEF4MphODO7adQlX8MYNuZCR7D0I24PcjYHDPussnTeyjVcyZA7Sd9qqBxuHZBVtbIxAmSi0xZCKw4kzjUXWKpxD77vpi6SbqHAZDZD';
-
-module.exports = {
-  messengerPageToken: messengerPageToken,
-  messengerVerifyToken: 'perrytheplatypus'
-};
