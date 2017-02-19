546658d9687afc8ad1eec45e867231d789e6c78e
ellenluo <ellenyilan.luo@gmail.com>
2017-02-19 00:11:24 -0800

    Added filtering functionality

--- website/js/main.js
+++ website/js/main.js
@@ -5,13 +5,16 @@ jQuery(document).ready(function () {
     var loaded = [];
     var pets = [];
 
+    var animal = '';
+    var zip = 94720;
+    var age = '';
+    var gender = '';
+
     //get all the pages link inside the #pages div and fill an array
     $('#pages a').each(function (index) {
         pages[index] = $(this).attr('href');
         loaded[$(this).attr('href')] = 0;
     });
-
-    console.log(pages);
 
     //on scroll gets when bottom of the page is reached and calls the function do load more content
     $(window).scroll(function (e) {
@@ -39,22 +42,233 @@ jQuery(document).ready(function () {
         }
     }
 
-    // get json data
-    $.ajax({
-        dataType: 'jsonp',
-        url: "http://api.petfinder.com/pet.find?key=150d5bff08a3c97e69c2417d726c084d&location=94720&format=json&count=100",
-        data: "",
-        success: function (data) {
-            pets = [];
-            $.each(data.petfinder.pets.pet, function (i, obj) {
-                pets.push(obj);
-            });
-            updateItems();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown);
-        }
+    // filter location button
+    $("#filter-zip-btn").click(function () {
+        zip = $("#filter-zip").val();
+        getUrl();
     });
+
+
+    // filter all
+    $("#all_animals").click(function () {
+        $(this).parent().parent().find('button').text('').html('All <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = '';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter barnyard
+    $("#barnyard").click(function () {
+        $(this).parent().parent().find('button').text('').html('Barnyard <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = 'barnyard';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter bird
+    $("#bird").click(function () {
+        $(this).parent().parent().find('button').text('').html('Bird <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = 'bird';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter cat
+    $("#cat").click(function () {
+        $(this).parent().parent().find('button').text('').html('Cat <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = 'cat';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter dog
+    $("#dog").click(function () {
+        $(this).parent().parent().find('button').text('').html('Dog <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = 'dog';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter horse
+    $("#horse").click(function () {
+        $(this).parent().parent().find('button').text('').html('Horse <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = 'horse';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter pig
+    $("#pig").click(function () {
+        $(this).parent().parent().find('button').text('').html('Pig <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = 'pig';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter reptile
+    $("#reptile").click(function () {
+        $(this).parent().parent().find('button').text('').html('Reptile <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = 'reptile';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter small furry
+    $("#small_furry").click(function () {
+        $(this).parent().parent().find('button').text('').html('Small Furry <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        animal = 'smallfurry';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter all
+    $("#all_ages").click(function () {
+        $(this).parent().parent().find('button').text('').html('All <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        age = '';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter baby
+    $("#baby").click(function () {
+        $(this).parent().parent().find('button').text('').html('Baby <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        age = 'baby';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter young
+    $("#young").click(function () {
+        $(this).parent().parent().find('button').text('').html('Young <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        age = 'young';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter adult
+    $("#adult").click(function () {
+        $(this).parent().parent().find('button').text('').html('Adult <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        age = 'adult';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter baby
+    $("#senior").click(function () {
+        $(this).parent().parent().find('button').text('').html('Senior <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        age = 'senior';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter all
+    $("#all_genders").click(function () {
+        $(this).parent().parent().find('button').text('').html('All <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        gender = '';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter male
+    $("#male").click(function () {
+        $(this).parent().parent().find('button').text('').html('Male <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        gender = 'M';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // filter female
+    $("#female").click(function () {
+        $(this).parent().parent().find('button').text('').html('Female <span class="caret"></span>');
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        gender = 'F';
+        getUrl();
+    }).on("mouseover", function () {
+        $(this).css('cursor', 'pointer');
+    });
+
+    // gets API call url
+    function getUrl() {
+        var url = "http://api.petfinder.com/pet.find?key=150d5bff08a3c97e69c2417d726c084d&format=json&count=100&location=" + zip;
+        if (animal != '') {
+            url += '&animal=' + animal;
+        }
+        if (age != '') {
+            url += '&age=' + age;
+        }
+        if (gender != '') {
+            url += '&sex=' + gender;
+        }
+        fetchData(url);
+    }
+
+    // get pets json data
+    function fetchData(url) {
+        $.ajax({
+            dataType: 'jsonp',
+            url: url,
+            data: "",
+            success: function (data) {
+                pets = [];
+                $.each(data.petfinder.pets.pet, function (i, obj) {
+                    pets.push(obj);
+                });
+                updateItems();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+    }
 
     // update pet items
     function updateItems() {
@@ -113,5 +327,7 @@ jQuery(document).ready(function () {
         console.log(id);
         localStorage.setItem("current_pet_id", id);
     }
+
+    getUrl();
 
 });
