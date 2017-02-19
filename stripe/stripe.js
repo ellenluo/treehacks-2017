ac3e422d2d1530df4fcb3f493af9a1ad11a55b57
Julia Luo <juliazhiyuan.luo@gmail.com>
2017-02-18 22:30:24 -0800

    added stripe form

---
+++ stripe/stripe.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function () {
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_1ikCSptUI6ji5bnIwVIL3CU6',
+    image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+    locale: 'auto',
+    token: function(token) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+    }
+  });
+  
+  $("#enter_pledge").keyup(function(e){
+    if(e.keyCode == 13){
+        console.log("hi");
+        e.preventDefault();
+        console.log("woo");
+        $('#error_explanation').html('');
+
+        var amount = $('#enter_pledge').val();
+        amount = amount.replace(/\$/g, '').replace(/\,/g, '')
+        amount = parseFloat(amount);
+
+        if (isNaN(amount)) {
+            $('#error_explanation').html('<p>Please enter a valid amount in USD ($).</p>');
+        }
+        else if (amount < 1.00) {
+            $('#error_explanation').html('<p>Sorry, pledge amount must be at least $1.</p>');
+        }
+        else {
+            amount = amount * 100; // Needs to be an integer!
+            handler.open({
+                name: 'Petreon',
+                description: 'Pledge to support this pet',
+                zipCode: true,
+                amount: Math.round(amount)
+            })
+        }
+     }
+  });
+  
+  window.addEventListener('popstate', function() {
+    handler.close();
+  });
+});
