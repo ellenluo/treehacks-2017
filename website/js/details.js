eb905de0dabd0b4a936952feef954fd04bb0f7d2
ellenluo <ellenyilan.luo@gmail.com>
2017-02-18 19:47:21 -0800

    Fixes.

--- website/js/details.js
+++ website/js/details.js
@@ -16,24 +16,21 @@ jQuery(document).ready(function () {
         });
     }
 
+    // populate pet data
     function populateData(pet) {
-        // set photo
-        /*var photo = '';
-        if (pet.media.photos != null) {
-            var photos = [];
-
-            $.each(pet.media.photos.photo, function (i, obj) {
-                if (obj['@size'] == 'x') {
-                    photo = obj.$t;
-                }
-                photos.push(obj);
-            });
-        }
-        $(obj).find(".img").css("background-image", "url(" + photo + ")");*/
-
-        // find breeds
         console.log(pet);
 
+        // set photo
+        var photos = [];
+        if (pet.media.photos != null) {
+            $.each(pet.media.photos.photo, function (i, obj) {
+                if (obj['@size'] == 'x') {
+                    photos.push(obj);
+                }
+            });
+        }
+
+        // find breeds
         var breeds = [];
         var JsonBreeds = pet.breeds.breed;
         if (isArray(JsonBreeds)) {
@@ -44,14 +41,56 @@ jQuery(document).ready(function () {
             breeds = [JsonBreeds.$t];
         }
 
+        // populate pet details
         $('.overlay').find('h1').text(pet.name.$t);
         $('#animal').text("Animal: " + pet.animal.$t);
         $('#breed').text("Breed(s): " + breeds.join(", "));
         $('#age').text("Age: " + pet.age.$t);
         $('#size').text("Size: " + pet.size.$t);
         $('#sex').text("Sex: " + pet.sex.$t);
-        $('#sex').text("Sex: " + pet.sex.$t);
+
+        // populate shelter info
+        var shelterId = pet.shelterId.$t;
+        loadShelter(shelterId);
+
     }
+
+    // load shelter data
+    function loadShelter(shelterId) {
+        $.ajax({
+            dataType: 'jsonp',
+            url: "http://api.petfinder.com/shelter.get?format=json&key=150d5bff08a3c97e69c2417d726c084d&id=" + shelterId,
+            data: "",
+            success: function (data) {
+                var shelter= data.petfinder.shelter;
+                populateShelter(shelter);
+                console.log(shelter);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+    }
+
+    // populate shelter data
+    function populateShelter(shelter) {
+        console.log("done");
+        $('#shelter-name').text("Name: " + shelter.name.$t);
+        $('#shelter-phone').text("Phone: " + shelter.phone.$t);
+        $('#shelter-email').text("Email: " + shelter.email.$t);
+    }
+
+    // populate photo carousel
+    /*function populateCarousel(photos) {
+        for(var i=0 ; i< m.length ; i++) {
+            $('<div class="item"><img src="'+m[i]+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+            $('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+
+        }
+        $('.item').first().addClass('active');
+        $('.carousel-indicators > li').first().addClass('active');
+        $('#carousel-example-generic').carousel();
+    }*/
 
     // check if JSON is object or array
     function isArray(what) {
