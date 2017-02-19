843f66ba797ad5e4569dc356f21dd8a27f16ab3a
Matthew Soh <matthewsoh.cw@gmail.com>
2017-02-18 18:19:56 -0800

    Pulls pets from database

--- src/bot/main.py
+++ src/bot/main.py
@@ -3,10 +3,18 @@ import fbmq
 from fbmq import Attachment, Template, QuickReply, Page
 import json
 import requests
+import sqlalchemy
+import psycopg2
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 verify_token = "perrytheplatypus"
 PAGE_ACCESS_TOKEN = "EAAJjL86GCNQBAKofF9TZC7vtTSUQXXkt6fCCirOPg7ZC2HDZAzfKhvfAcxY4kTQ0IFpA1K6OXVxRrK85x5vRvmmsECPIzgZAZBFa5ObuozDa9tq9XxXc3HXWJCT6ogYEEj89r1bPx3K4nTRXMtyCNMThiv7pwCEOk1ZAF5ZBxULZCwZDZD"
 default_img  = "http://photos.petfinder.com/photos/pets/28854051/3/?bust=1432951081&width=500&-x.jpg"
+
+engine = None
+
 def lambda_handler(event, context):
     if 'params' in event.keys():
         params = event['params']['querystring']
@@ -25,10 +33,11 @@ def lambda_handler(event, context):
 
             page = fbmq.Page(PAGE_ACCESS_TOKEN)
             if message == 'next':
-                page.send(sender, "Hello!")
-                page.send(sender, "You said {}.".format(message))
-                attachment = Attachment.Image(default_img)
-                page.send(sender, attachment)
+                connectDb()
+                pet = getRandomPet()
+                #attachment = Attachment.Image(default_img)
+                #page.send(sender, attachment)
+                page.send(sender, "This pet is {} (id = {}).".format(pet.name, pet.pet_id))
                 page.send(sender, "Aww.")
         else:
             print(messaging)
@@ -42,3 +51,35 @@ def lambda_handler(event, context):
     };
 
     return response
+
+Base = declarative_base()
+class Pet(Base):
+    __tablename__ = 'pets'
+    id = Column(Integer, primary_key=True)
+    pet_id = Column(Integer, unique=True, nullable=False)                              
+    name = Column(String(1000), nullable=False) 
+
+    def __init__(self, pet_id, name):
+        self.pet_id = pet_id
+        self.name = name
+
+def connectDb():
+    global engine
+    user = 'perry'
+    password = 'theplatypus'
+    host = 'petron.ckcrv4vmtlra.us-west-2.rds.amazonaws.com'
+    port = 5432
+    database = 'petron'
+
+    engine = create_engine('postgresql+psycopg2://{}:{}@{}:{}/{}'.format(user, password, host, port, database))
+
+def getRandomPet():
+    import random
+    ct = 1000
+    idx = random.randint(0, ct -1)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(Pet)
+    return query.offset(idx).first()
+
+
